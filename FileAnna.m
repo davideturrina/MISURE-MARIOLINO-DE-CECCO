d0de4d148394d1d07cdabd311e17ece6175c142d
Utente <you@example.com>
2023-03-27 11:56:22 +0200

    ciao

--- FileAnna.m
+++ FileAnna.m
@@ -4,8 +4,8 @@ clear
 
 % NOTA: ,,,
 % - nel file "*.jpeg" c'è una foto scattata con smartphone
-% - nel file "*.tif" i dati misurati con laser a triangolazioneasd
-% in forma di una immagine con valori di ciascun pixel pari u7j7j7
+% - nel file "*.tif" i dati misurati con laser a triangolazione
+% in forma di una immagine con valori di ciascun pixel pari u7j7j
 % alle quote lungo la verticale misurate mediante laser
 
 % NOTA: inserire i percorsi corretti su vostro PC
